d6c20f9b169826ac37fb81a4da8c561fe11a81c6
BlessingOkora <moraablessing082@gmail.com>
2024-12-12 13:46:03 +0300

    builds getElementById

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,21 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    stack = [self.root]
+    while stack:
+        current_node = stack.pop()
+        if current_node.get('id') == id:
+            return current_node
+        stack.extend(current_node.get('children', []))
+    return None
+  
+  def get_element_by_id_bfs(self, id):
+    queue = [self.root]
+    while queue:
+        current_node = queue.pop(0)
+        if current_node.get('id') == id:
+            return current_node
+        queue.extend(current_node.get('children', []))
+    return None
+     
+        
